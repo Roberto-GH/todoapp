584fbde70f560be44e16c29bab1b6116053881e1
Roberto Londoño <roberthdrums@gmail.com>
2023-10-03 16:25:25 -0500

    📝 docs(.gitignore): añadir reglas de ignorar archivos y directorios comunes 📝 docs(.mvn/wrapper/maven-wrapper.jar): añadir archivo maven-wrapper.jar 📝 docs(.mvn/wrapper/maven-wrapper.properties): añadir archivo maven-wrapper.properties
    📦 chore(mvnw): añadir archivo mvnw
    
    Se añade el archivo mvnw al proyecto. Este archivo es un script de inicio de Maven Wrapper, que permite ejecutar Maven sin tener que instalarlo de forma global en el sistema. El archivo mvnw contiene las configuraciones necesarias para ejecutar Maven, incluyendo la ubicación de Java y las variables de entorno necesarias.
    
    📦 chore(mvnw.cmd): añadir archivo mvnw.cmd para facilitar la ejecución de Maven
    📦 chore(pom.xml): añadir archivo pom.xml con la configuración del proyecto Maven
    📦 chore(TodoappApplication.java): añadir archivo TodoappApplication.java con la clase principal de la aplicación
    📦 chore(application.properties): añadir archivo application.properties para la configuración de la aplicación
    📦 chore(TodoappApplicationTests.java): añadir archivo TodoappApplicationTests.java con las pruebas unitarias de la aplicación

---
+++ src/test/java/com/rln/todoapp/TodoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rln.todoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
